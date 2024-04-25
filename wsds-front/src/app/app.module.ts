c951e53f8411f09477c41a6ffce9f722c108553b
RauPro <00012119@uca.edu.sv>
2024-04-24 18:17:35 -0600

    Styles for search engine

--- wsds-front/src/app/app.module.ts
+++ wsds-front/src/app/app.module.ts
@@ -5,27 +5,60 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { BannerComponent } from './components/Banner/banner.component';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormField, MatFormFieldModule, MatLabel} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from "@angular/material/autocomplete";
+import {
+  MatDatepickerModule,
+  MatDatepickerToggle,
+  MatDateRangeInput,
+  MatDateRangePicker
+} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    BannerComponent
+    BannerComponent,
+    NavBarComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    NgbModule
+    MatButtonModule,
+    NgbModule,
+    MatFormField,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIcon,
+    MatAutocomplete,
+    MatOption,
+    MatAutocompleteTrigger,
+    ReactiveFormsModule,
+    MatDateRangeInput,
+    MatDatepickerToggle,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDateRangePicker,
+    MatSlideToggle
   ],
   providers: [
     provideClientHydration(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    MatDatepickerModule
   ],
   bootstrap: [AppComponent]
 })
