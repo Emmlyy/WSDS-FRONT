f15091a15a9ec1149ce496a2e13bd10c6de50201
mayiPalacios <00018619@uca.edu.sv>
2024-04-28 12:32:18 -0600

    add redirect btn

--- wsds-front/src/app/components/Ficha/ficha.component.ts
+++ wsds-front/src/app/components/Ficha/ficha.component.ts
@@ -1,7 +1,7 @@
-import {Component, Input, OnInit } from '@angular/core';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 /**
@@ -11,7 +11,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   selector: 'dialog-content',
   templateUrl: './dialog-content.html',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule,MatDialogModule],
+  imports: [MatButtonModule, MatCardModule, MatDialogModule],
 })
 export class DialogContentFicha {
   constructor(public dialog: MatDialog) {}
@@ -19,12 +19,11 @@ export class DialogContentFicha {
   openDialog() {
     const dialogRef = this.dialog.open(FichaContentDialog);
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
   }
 }
-
 
 @Component({
   selector: 'dialog-content-example-dialog',
@@ -45,7 +44,7 @@ export class ButtonReportComponent {
   openDialog() {
     const dialogRef = this.dialog.open(NewsDetailsComponent);
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
   }
@@ -54,29 +53,69 @@ export class ButtonReportComponent {
 @Component({
   selector: 'app-news-details',
   templateUrl: './ficha-form.componet.html',
-  styleUrls: ['./ficha-form.component.css']
+  styleUrls: ['./ficha-form.component.css'],
 })
 export class NewsDetailsComponent implements OnInit {
   fichaForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     // Define los campos del formulario y su estado inicial
     this.fichaForm = this.fb.group({
-      description: [{ value: 'La noticia describe un homicidio, ya que contiene las palabras clave \"homicidio\" y \"feminicidio\".', disabled: true }],
+      description: [
+        {
+          value:
+            'La noticia describe un homicidio, ya que contiene las palabras clave "homicidio" y "feminicidio".',
+          disabled: true,
+        },
+      ],
       classification: [{ value: 'Homicidio', disabled: true }],
-      title:  [{ value: 'Detienen a sujeto por golpear e intentar matar a su pareja con un cuchillo', disabled: true }],
-      summary:  [{ value: 'La policía detuvo a un hombre por presuntamente intentar asesinar a su compañera de vida con un cuchillo.', disabled: true }],
-      location_of_incident:  [{ value: 'No se especifica en la noticia.', disabled: true }],
+      title: [
+        {
+          value:
+            'Detienen a sujeto por golpear e intentar matar a su pareja con un cuchillo',
+          disabled: true,
+        },
+      ],
+      summary: [
+        {
+          value:
+            'La policía detuvo a un hombre por presuntamente intentar asesinar a su compañera de vida con un cuchillo.',
+          disabled: true,
+        },
+      ],
+      location_of_incident: [
+        { value: 'No se especifica en la noticia.', disabled: true },
+      ],
       sources: [{ value: 'No se indican en la noticia.', disabled: true }],
-      themes:  [{ value: 'Violencia contra las mujeres, feminicidio', disabled: true }],
-      violative_facts:  [{ value: 'El hombre golpeó a su mujer y posteriormente intento asesinarla con un cuchillo.', disabled: true }],
-      hypothesis_of_facts:  [{ value: 'No se especifica en la noticia.', disabled: true }],
-      vulnerable_population:  [{ value: 'No se especifica en la noticia.', disabled: true }],
+      themes: [
+        { value: 'Violencia contra las mujeres, feminicidio', disabled: true },
+      ],
+      violative_facts: [
+        {
+          value:
+            'El hombre golpeó a su mujer y posteriormente intento asesinarla con un cuchillo.',
+          disabled: true,
+        },
+      ],
+      hypothesis_of_facts: [
+        { value: 'No se especifica en la noticia.', disabled: true },
+      ],
+      vulnerable_population: [
+        { value: 'No se especifica en la noticia.', disabled: true },
+      ],
       type_of_weapon: [{ value: 'Cuchillo', disabled: true }],
-      victims:  [{ value: 'La víctima no se identifica en la noticia.', disabled: true }],
-      perpetrator_or_suspected_aggressor:  [{ value: 'El hombre que perpetró el homicidio no se identifica en la noticia', disabled: true }],
+      victims: [
+        { value: 'La víctima no se identifica en la noticia.', disabled: true },
+      ],
+      perpetrator_or_suspected_aggressor: [
+        {
+          value:
+            'El hombre que perpetró el homicidio no se identifica en la noticia',
+          disabled: true,
+        },
+      ],
     });
   }
 
--- wsds-front/src/app/components/search/search.component.ts
+++ wsds-front/src/app/components/search/search.component.ts
@@ -1,14 +1,12 @@
-import {Component} from '@angular/core';
-import {GemmaService} from '../../services/gemma.service';
-import {map, Observable, startWith} from "rxjs";
-import {FormControl} from "@angular/forms";
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
+import { Component } from '@angular/core';
+import { GemmaService } from '../../services/gemma.service';
+import { map, Observable, startWith } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
-
-import global from "./../../mocks/global"
-interface OnInit {
-}
+import global from './../../mocks/global';
+interface OnInit {}
 
 @Component({
   selector: 'app-search',
@@ -17,75 +15,131 @@ interface OnInit {
   //YourDialog: '../Ficha/ficha.component.html'
 })
 export class SearchComponent implements OnInit {
-
   searchControl = new FormControl('');
-  townsControl = new FormControl('')
+  townsControl = new FormControl('');
   departmentsControl = new FormControl('');
   results: any[] = [];
   options: string[] = ['Homicidio', 'Feminicidio', 'Asesinato'];
   departments = [
-    { name: 'Ahuachapán', towns: ['Ahuachapán Norte', 'Ahuachapán Centro', 'Ahuachapán Sur'] },
-    { name: 'San Salvador', towns: ['San Salvador Norte', 'San Salvador Oeste', 'San Salvador Este', 'San Salvador Centro', 'San Salvador Sur'] },
-    { name: 'La Libertad', towns: ['La Libertad Norte', 'La Libertad Centro', 'La Libertad Oeste', 'La Libertad Este', 'La Libertad Costa', 'La Libertad Sur'] },
-    { name: 'Chalatenango', towns: ['Chalatenango Norte', 'Chalatenango Centro', 'Chalatenango Sur'] },
+    {
+      name: 'Ahuachapán',
+      towns: ['Ahuachapán Norte', 'Ahuachapán Centro', 'Ahuachapán Sur'],
+    },
+    {
+      name: 'San Salvador',
+      towns: [
+        'San Salvador Norte',
+        'San Salvador Oeste',
+        'San Salvador Este',
+        'San Salvador Centro',
+        'San Salvador Sur',
+      ],
+    },
+    {
+      name: 'La Libertad',
+      towns: [
+        'La Libertad Norte',
+        'La Libertad Centro',
+        'La Libertad Oeste',
+        'La Libertad Este',
+        'La Libertad Costa',
+        'La Libertad Sur',
+      ],
+    },
+    {
+      name: 'Chalatenango',
+      towns: ['Chalatenango Norte', 'Chalatenango Centro', 'Chalatenango Sur'],
+    },
     { name: 'Cuscatlán', towns: ['Cuscatlán Norte', 'Cuscatlán Sur'] },
     { name: 'Cabañas', towns: ['Cabañas Este', 'Cabañas Oeste'] },
     { name: 'La Paz', towns: ['La Paz Oeste', 'La Paz Centro', 'La Paz Este'] },
     { name: 'La Unión', towns: ['La Unión Norte', 'La Unión Sur'] },
-    { name: 'Usulután', towns: ['Usulután Norte', 'Usulután Este', 'Usulután Oeste'] },
-    { name: 'Sonsonate', towns: ['Sonsonate Norte', 'Sonsonate Centro', 'Sonsonate Este', 'Sonsonate Oeste'] },
-    { name: 'Santa Ana', towns: ['Santa Ana Norte', 'Santa Ana Centro', 'Santa Ana Este', 'Santa Ana Oeste'] },
+    {
+      name: 'Usulután',
+      towns: ['Usulután Norte', 'Usulután Este', 'Usulután Oeste'],
+    },
+    {
+      name: 'Sonsonate',
+      towns: [
+        'Sonsonate Norte',
+        'Sonsonate Centro',
+        'Sonsonate Este',
+        'Sonsonate Oeste',
+      ],
+    },
+    {
+      name: 'Santa Ana',
+      towns: [
+        'Santa Ana Norte',
+        'Santa Ana Centro',
+        'Santa Ana Este',
+        'Santa Ana Oeste',
+      ],
+    },
     { name: 'San Vicente', towns: ['San Vicente Norte', 'San Vicente Sur'] },
-    { name: 'San Miguel', towns: ['San Miguel Norte', 'San Miguel Centro', 'San Miguel Oeste'] },
-    { name: 'Morazán', towns: ['Morazán Norte', 'Morazán Sur'] }
+    {
+      name: 'San Miguel',
+      towns: ['San Miguel Norte', 'San Miguel Centro', 'San Miguel Oeste'],
+    },
+    { name: 'Morazán', towns: ['Morazán Norte', 'Morazán Sur'] },
   ];
   filteredOptions: Observable<string[]> | undefined;
   filteredOptionsDepartments: Observable<string[]> | undefined;
   filteredOptionsTowns: Observable<string[]> | undefined;
-  currentDepartment: {name: string, towns: string[]} | null | undefined = null;
+  currentDepartment: { name: string; towns: string[] } | null | undefined =
+    null;
   indicators = [
-    "Víctima",
-    "Agresor/a",
-    "Edad de la víctima y del agresor",
-    "Hechos vulneratorios",
-    "Contexto del hecho",
-    "Lugar del hecho",
-    "Arma",
-    "Estado del agresor/a después del hecho",
-    "Homicidios sin contextos"
+    'Víctima',
+    'Agresor/a',
+    'Edad de la víctima y del agresor',
+    'Hechos vulneratorios',
+    'Contexto del hecho',
+    'Lugar del hecho',
+    'Arma',
+    'Estado del agresor/a después del hecho',
+    'Homicidios sin contextos',
   ];
   panelOpenState: boolean = false;
   iconMapper = new Map([
     ['diario.elmundo.sv', './../../assets/elmundo.png'],
     ['diariocolatino.com', './../../assets/colatino.png'],
     ['diarioelsalvador.com', './../../assets/elsalvador.png'],
-  ])
-  news: {title: string, text: string, source: string, tag:string}[] | null | undefined = null;
+  ]);
+  news:
+    | {
+        title: string;
+        text: string;
+        source: string;
+        tag: string;
+        url: string;
+      }[]
+    | null
+    | undefined = null;
 
-  constructor(private gemmaService: GemmaService) { }
-  ngOnInit(){
+  constructor(private gemmaService: GemmaService) {}
+  ngOnInit() {
     this.filteredOptions = this.searchControl.valueChanges.pipe(
       startWith(''),
-      map(value => this._filter(value || '')),
+      map((value) => this._filter(value || ''))
     );
     this.filteredOptionsDepartments = this.departmentsControl.valueChanges.pipe(
       startWith(''),
-      map(value => this._filterDepartments(value || '')),
+      map((value) => this._filterDepartments(value || ''))
     );
-    this.departmentsControl.valueChanges.subscribe(value => {
-      this.currentDepartment = this.departments.find(obj => obj.name == value)
-      console.log(this.currentDepartment)
-    })
-
+    this.departmentsControl.valueChanges.subscribe((value) => {
+      this.currentDepartment = this.departments.find(
+        (obj) => obj.name == value
+      );
+      console.log(this.currentDepartment);
+    });
   }
   onSearch(): void {
-    console.log(this.searchControl.value)
     if (this.searchControl.value) {
       this.gemmaService.searchData(this.searchControl.value).subscribe(
-        data => {
-          this.news = data
+        (data) => {
+          this.news = data;
         },
-        error => {
+        (error) => {
           console.error('Error al obtener datos:', error);
           this.results = [];
         }
@@ -98,20 +152,21 @@ export class SearchComponent implements OnInit {
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
   }
 
-  private _filterDepartments(value: string): string[]  {
+  private _filterDepartments(value: string): string[] {
     const filterValue = value.toLowerCase();
-    return this.departments.filter(option => option.name.toLowerCase().includes(filterValue)).map(el => el.name);
+    return this.departments
+      .filter((option) => option.name.toLowerCase().includes(filterValue))
+      .map((el) => el.name);
   }
 
   changeDepartments($event: Event) {
-    console.log(this.departmentsControl.value)
+    console.log(this.departmentsControl.value);
   }
-
-
-
 }
 
 /*
