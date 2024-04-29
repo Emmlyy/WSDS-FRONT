5e772eba507d0216b0f85b3264ae0913d1173313
RauPro <00012119@uca.edu.sv>
2024-04-29 17:16:55 -0600

    Add: disable by df

--- wsds-front/src/app/components/Indicadores/prompt.component.ts
+++ wsds-front/src/app/components/Indicadores/prompt.component.ts
@@ -5,7 +5,6 @@ interface Indicador {
   indicator_name: string;
   prompt: string;
   id: number;
-  disabled: boolean;
 }
 
 @Component({
@@ -20,6 +19,7 @@ export class IndicadoresComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.indicadoresForm = this.fb.group({});
   }
+  disableEdit: boolean = true;
 
   ngOnInit(): void {
     // Simulación de la obtención del JSON de indicadores desde algún servicio
@@ -28,73 +28,61 @@ export class IndicadoresComponent implements OnInit {
           "indicator_name": "Clasificación",
           "prompt": "Clasifica el tipo de incidente reportado",
           "id": 2,
-          "disabled": true,
         },
         {
           "indicator_name": "Título",
           "prompt": "Extrae el título de la noticia",
           "id": 3,
-          "disabled": true,
         },
         {
           "indicator_name": "Resumen",
           "prompt": "Proporciona un breve resumen de la noticia",
           "id": 4,
-          "disabled": true,
         },
         {
           "indicator_name": "Lugar de los Hechos",
           "prompt": "Dónde ocurrió el suceso",
           "id": 5,
-          "disabled": true,
         },
         {
           "indicator_name": "Fuentes",
           "prompt": "Cita las fuentes de información",
           "id": 6,
-          "disabled": true,
         },
         {
           "indicator_name": "Temas",
           "prompt": "Los temas principales tratados",
           "id": 7,
-          "disabled": true,
         },
         {
           "indicator_name": "Hechos Violatorios",
           "prompt": "Detalles específicos sobre la violación a la ley",
           "id": 8,
-          "disabled": true,
         },
         {
           "indicator_name": "Hipótesis de Hechos",
           "prompt": "Cualquier teoría o suposición presentada",
           "id": 9,
-          "disabled": true,
         },
         {
           "indicator_name": "Población Vulnerable",
           "prompt": "Grupos en riesgo mencionados",
           "id": 10,
-          "disabled": true,
         },
         {
           "indicator_name": "Tipo de Arma",
           "prompt": "Especifica el tipo de arma, si se menciona",
           "id": 11,
-          "disabled": true,
         },
         {
           "indicator_name": "Víctimas",
           "prompt": "Identifica a las víctimas, si es posible",
           "id": 12,
-          "disabled": true,
         },
         {
           "indicator_name": "Victimario o Presunto Agresor",
           "prompt": "Nombre del agresor, si se menciona",
           "id": 13,
-          "disabled": true,
         }
 
 
@@ -103,17 +91,13 @@ export class IndicadoresComponent implements OnInit {
     // Asignar el JSON de indicadores y crear FormControl para cada uno
     this.indicadores = jsonIndicadores;
     this.indicadores.forEach(indicador => {
-      this.indicadoresForm.addControl(indicador.id.toString(), this.fb.control(''));
+      this.indicadoresForm.addControl(indicador.id.toString(), this.fb.control({value: indicador.prompt, disabled: this.disableEdit}));
     });
   }
 
   // Método para habilitar/deshabilitar la edición del formulario
-  toggleFormEdit() {
 
-    if (this.indicadoresForm.enabled) {
-      this.indicadoresForm.disable();
-    } else {
-      this.indicadoresForm.enable();
-    }
+  toggleFormEdit() {
+    this.indicadoresForm.enabled ? this.indicadoresForm.disable() : this.indicadoresForm.enable();
   }
 }
