56eb4e65b747061bd2738f05214827d33b0f8861
RauPro <00012119@uca.edu.sv>
2024-05-09 18:37:31 -0600

    Change icons

--- wsds-front/src/app/app.component.ts
+++ wsds-front/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {LoaderService} from "./services/loader.service";
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,9 @@ import {LoaderService} from "./services/loader.service";
 export class AppComponent {
   title = 'wsds-front';
   loading = this.loaderService.isLoading;
+  messages = ['Extrayendo noticias...', 'Buscando noticias...', 'Scrapeando noticias...'];
+  currentMessage$ = interval(3000).pipe(
+    map(i => this.messages[i % this.messages.length])
+  );
   constructor(private loaderService: LoaderService) {}
 }
