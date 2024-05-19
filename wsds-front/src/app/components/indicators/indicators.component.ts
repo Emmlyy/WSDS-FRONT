eef2f205f1c7a9a8d298709cea3aa364d5f76986
RauPro <00012119@uca.edu.sv>
2024-05-19 16:11:54 -0600

    Styles for indicators

--- wsds-front/src/app/components/indicators/indicators.component.ts
+++ wsds-front/src/app/components/indicators/indicators.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {IndicatorService} from "../../services/indicator.service";
-import {IIndicatorEntry} from "../../interfaces/indicators.interface";
+import {IIndicator, IIndicatorEntry} from "../../interfaces/indicators.interface";
 import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
@@ -9,15 +9,25 @@ import {error} from "@angular/compiler-cli/src/transformers/util";
   styleUrl: './indicators.component.scss'
 })
 export class IndicatorsComponent {
-  currentSetting: string = "1";
+  currentSetting: string = "202405-1622-2128-85e71527-8fbf-4889-baa1-3ff5ad6194ec";
   constructor(private indicatorService: IndicatorService) {
   }
   indicatorsAvailable: IIndicatorEntry[] = []
+  currentIndicators!: IIndicatorEntry | undefined;
   ngOnInit(){
     this.indicatorService.getAllPromptEntryData().subscribe(items => {
+      console.log(items)
       this.indicatorsAvailable = items;
+      this.currentIndicators =  this.indicatorsAvailable.find(element =>
+        element.id == this.currentSetting)
     }, error => {
       console.log(error)
     })
+  }
+
+  changeSetting() {
+    console.log(this.currentSetting)
+    this.currentIndicators =  this.indicatorsAvailable.find(element =>
+      element.id == this.currentSetting)
   }
 }
